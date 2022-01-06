634a4960a8777ff71a5d0e967ac6965425999aa7
Turnerj <git@turnerj.com>
2022-01-07 00:34:38 +1030

    Add query message writing benchmark

---
+++ benchmarks/TurnerSoftware.DinoDNS.Benchmarks/QueryMessageWritingBenchmark.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using TurnerSoftware.DinoDNS.Protocol;
+
+namespace TurnerSoftware.DinoDNS.Benchmarks;
+
+[Config(typeof(CustomConfig))]
+[SimpleJob(RuntimeMoniker.Net60)]
+[MemoryDiagnoser]
+public class QueryMessageWritingBenchmark
+{
+	private byte[] Buffer = new byte[1024];
+
+	private DnsMessage DinoDNS_Message;
+	private DNS.Protocol.Request? DNS_Request;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		DinoDNS_Message = DnsMessage.CreateQuery(44124)
+			.WithQuestions(new Question[]
+			{
+				new()
+				{
+					Query = new LabelSequence("test.www.example.org"),
+					Type = DnsQueryType.A,
+					Class = DnsClass.IN
+				}
+			});
+
+		DNS_Request = new DNS.Protocol.Request()
+		{
+			Id = 44124,
+			OperationCode = DNS.Protocol.OperationCode.Query
+		};
+		DNS_Request.Questions.Add(new DNS.Protocol.Question(
+			new DNS.Protocol.Domain("test.www.example.org")
+		));
+	}
+
+	[Benchmark(Baseline = true)]
+	public void DinoDNS()
+	{
+		new DnsProtocolWriter(Buffer.AsMemory())
+			.AppendMessage(DinoDNS_Message);
+	}
+
+	[Benchmark]
+	public void Kapetan_DNS()
+	{
+		DNS_Request!.ToArray();
+	}
+}
