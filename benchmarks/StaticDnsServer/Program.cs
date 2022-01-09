a0ca56b1b2d62c80dc03e9a5b24a27f4075662f7
Turnerj <git@turnerj.com>
2022-01-09 23:14:55 +1030

    Add auto-close to static DNS server

--- benchmarks/StaticDnsServer/Program.cs
+++ benchmarks/StaticDnsServer/Program.cs
@@ -4,21 +4,35 @@ using TurnerSoftware.DinoDNS;
 using TurnerSoftware.DinoDNS.Protocol;
 
 const int DEFAULT_DNS_SERVER_PORT = 53;
+const int TIMEOUT_IN_SECONDS = 8;
 
 Console.WriteLine("Full Stack DNS Benchmark Server");
+Console.WriteLine("===============================");
 Console.WriteLine("This is designed to respond as fast as possible with a static payload for a DNS query.");
+Console.WriteLine($"This server will end within {TIMEOUT_IN_SECONDS} seconds of the last request.");
 
 using var udpClient = new UdpClient(DEFAULT_DNS_SERVER_PORT);
 var endpoint = new IPEndPoint(0, 0);
+var cancellationSource = new CancellationTokenSource();
+cancellationSource.CancelAfter(TimeSpan.FromSeconds(TIMEOUT_IN_SECONDS));
 
 var exampleData = GenerateExampleData();
 
 Console.WriteLine("Now listening for DNS requests...");
 
-while (true)
+try
 {
-	_ = udpClient.Receive(ref endpoint);
-	udpClient.Send(exampleData.Span, endpoint);
+	while (true)
+	{
+		var result = await udpClient.ReceiveAsync(cancellationSource.Token);
+		udpClient.Send(exampleData.Span, result.RemoteEndPoint);
+		cancellationSource = new CancellationTokenSource();
+		cancellationSource.CancelAfter(TimeSpan.FromSeconds(TIMEOUT_IN_SECONDS));
+	}
+}
+catch (OperationCanceledException)
+{
+	Console.WriteLine("Benchmark server has ended.");
 }
 
 static ReadOnlyMemory<byte> GenerateExampleData()
