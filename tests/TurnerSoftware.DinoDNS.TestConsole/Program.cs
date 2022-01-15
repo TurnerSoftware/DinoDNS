8710025ca21c18c5879b861015ed92e1d226d6f1
Turnerj <git@turnerj.com>
2022-01-16 01:12:26 +1030

    Refactor the DNS test console

--- tests/TurnerSoftware.DinoDNS.TestConsole/Program.cs
+++ tests/TurnerSoftware.DinoDNS.TestConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using TurnerSoftware.DinoDNS;
 using TurnerSoftware.DinoDNS.Protocol;
 using System.Net.Sockets;
+using System.Net;
+using System.Diagnostics;
 
 Console.WriteLine(@"QDCOUNT: Question record count
 ANCOUNT: Answer record count
@@ -12,81 +14,54 @@ ARCOUNT: Additional record count
 // Core RFC for DNS: http://www.networksorcery.com/enp/rfc/rfc1035.txt
 // https://datatracker.ietf.org/doc/html/rfc1035
 
-
-//ShowDecode();
-
 await RunClientAsync();
 
-static void ShowDecode()
-{
-	var validBytes = new byte[]
-	{
-		0xc0, 0x0c, 0x00, 0x05, 0x00, 0x01, 0x00, 0x00,
-		0x04, 0x9a, 0x00, 0x27, 0x06, 0x61, 0x73, 0x69,
-		0x6d, 0x6f, 0x76, 0x06, 0x76, 0x6f, 0x72, 0x74,
-		0x65, 0x78, 0x04, 0x64, 0x61, 0x74, 0x61, 0x0e,
-		0x74, 0x72, 0x61, 0x66, 0x66, 0x69, 0x63, 0x6d,
-		0x61, 0x6e, 0x61, 0x67, 0x65, 0x72, 0x03, 0x6e,
-		0x65, 0x74, 0x00
-	};
-	var myBytes = new byte[]
-	{
-		0x00, 0x01, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x06, 0x76, 0x6f, 0x72,
-		0x74, 0x65, 0x78, 0x04, 0x64, 0x61, 0x74, 0x61,
-		0x09, 0x6d, 0x69, 0x63, 0x72, 0x6f, 0x73, 0x6f,
-		0x66, 0x74, 0x03, 0x63, 0x6f, 0x6d, 0x00, 0x01,
-		0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-	};
-	var testBytes = new byte[]
-	{
-		0x75, 0xea, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x06, 0x76, 0x6f, 0x72,
-		0x74, 0x65, 0x78, 0x04, 0x64, 0x61, 0x74, 0x61,
-		0x09, 0x6d, 0x69, 0x63, 0x72, 0x6f, 0x73, 0x6f,
-		0x66, 0x74, 0x03, 0x63, 0x6f, 0x6d, 0x00, 0x01,
-		0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-	};
-
-	Console.WriteLine("Theirs");
-	ProcessRequest(validBytes);
-	Console.WriteLine("Mine");
-	ProcessRequest(myBytes);
-	//Console.WriteLine("Test");
-	//ProcessRequest(testBytes);
-}
 static async ValueTask RunClientAsync()
 {
-	using var remoteServer = new UdpClient();
-	remoteServer.Connect("192.168.0.11", 53);
-	//remoteServer.Connect("1.1.1.1", 53);
-
-	byte[] buffer = new byte[1024];
-	var data = WriteClientTestData(buffer);
-
-	//Console.WriteLine("Request");
-	//WriteBytes(buffer.AsSpan()[..50]);
-
-	await remoteServer.SendAsync(data);
-	var remoteResult = await remoteServer.ReceiveAsync();
-	ProcessResponse(remoteResult.Buffer);
-
-
-	static ReadOnlyMemory<byte> WriteClientTestData(Memory<byte> destination)
+	var random = new Random();
+	var dnsClient = new DnsClient(new NameServer[]
 	{
-		var message = DnsMessage.CreateQuery(1, Opcode.Query, RecursionDesired.Yes)
+		new(IPAddress.Parse("1.1.1.1"), ConnectionType.Udp)
+	}, DnsClientOptions.Default);
+
+	var stopwatch = new Stopwatch();
+	while (true)
+	{
+		Console.Write("Query: ");
+		var query = Console.ReadLine();
+		if (query is null)
+		{
+			break;
+		}
+
+		QueryType:
+		Console.Write("Type: ");
+		var queryType = Console.ReadLine();
+		if (queryType is null)
+		{
+			continue;
+		}
+		
+		if (!Enum.TryParse(typeof(DnsQueryType), queryType, out var dnsQueryType))
+		{
+			Console.WriteLine("Invalid query type! (eg. A, CNAME, TXT)");
+			goto QueryType;
+		}
+
+		var message = DnsMessage.CreateQuery((ushort)random.Next(ushort.MaxValue))
 			.WithQuestions(new Question[]
 			{
-				new()
-				{
-					Query = new LabelSequence("vortex.data.microsoft.com"),
-					Type = DnsQueryType.A,
-					Class = DnsClass.IN
-				}
+				new Question(new LabelSequence(query), (DnsQueryType)dnsQueryType!, DnsClass.IN)
 			});
 
-		var writer = new DnsProtocolWriter(destination);
-		return writer.AppendMessage(message).GetWrittenBytes();
+		stopwatch.Restart();
+		var result = await dnsClient.SendAsync(message);
+		stopwatch.Stop();
+		PrintMessage(result);
+
+		Console.WriteLine("Time (ms): {0}", stopwatch.ElapsedMilliseconds);
+		Console.WriteLine();
+		Console.WriteLine();
 	}
 }
 
@@ -98,77 +73,42 @@ static async ValueTask RunServerAsync()
 	while (true)
 	{
 		var result = await localClient.ReceiveAsync();
-		ProcessRequest(result.Buffer);
+		new DnsProtocolReader(result.Buffer.AsMemory())
+			.ReadMessage(out var requestMessage);
+		PrintMessage(requestMessage);
 
 		await remoteServer.SendAsync(result.Buffer);
 		var remoteResult = await remoteServer.ReceiveAsync();
-		ProcessResponse(remoteResult.Buffer);
+
+		new DnsProtocolReader(result.Buffer.AsMemory())
+			.ReadMessage(out var responseMessage);
+		PrintMessage(responseMessage);
 
 		await localClient.SendAsync(remoteResult.Buffer, result.RemoteEndPoint);
 	}
 }
 
-static void ProcessRequest(byte[] buffer)
+static void PrintMessage(DnsMessage message)
 {
-	SeekableReadOnlyMemory<byte> memory = buffer.AsMemory();
-	var reader = new DnsProtocolReader(memory)
-		.ReadMessage(out var message);
-
 	Console.WriteLine(message.Header.ToString());
 
-	for (var i = 0; i < message.Questions.Length; i++)
+	foreach (var question in message.Questions)
 	{
-		var question = message.Questions[i];
-		Console.WriteLine(question.ToString());
-	}
-}
-
-static void ProcessResponse(byte[] buffer)
-{
-	SeekableReadOnlyMemory<byte> memory = buffer.AsMemory();
-	var reader = new DnsProtocolReader(memory)
-		.ReadMessage(out var message);
-
-	Console.WriteLine(message.Header.ToString());
-
-	//Console.WriteLine("Response");
-	//WriteBytes(seekableSpan.Span);
-
-	for (var i = 0; i < message.Questions.Length; i++)
-	{
-		var question = message.Questions[i];
 		Console.WriteLine(question.ToString());
 	}
 
-	for (var i = 0; i < message.Answers.Length; i++)
+	foreach (var resourceRecord in message.Answers)
 	{
-		var answer = message.Answers[i];
-		Console.WriteLine(answer.ToString());
+		Console.WriteLine(resourceRecord.ToString());
 	}
-}
 
-static void WriteBytes(ReadOnlySpan<byte> value)
-{
-	for (var i = 0; i < value.Length; i++)
+	foreach (var resourceRecord in message.Authorities)
 	{
-		WriteByte(value[i]);
-
-		if (i + 1 < value.Length)
-		{
-			Console.Write(' ');
-			WriteByte(value[++i]);
-		}
-
-		Console.WriteLine();
+		Console.WriteLine(resourceRecord.ToString());
 	}
-}
 
-static void WriteByte(byte value)
-{
-	Console.Write(Convert.ToString(value, 2).PadLeft(8, '0'));
-	Console.Write(" [");
-	Console.Write(Convert.ToString(value, 10).PadLeft(5, ' '));
-	Console.Write('/');
-	Console.Write((char)value);
-	Console.Write(']');
+	foreach (var resourceRecord in message.AdditionalRecords)
+	{
+		Console.WriteLine(resourceRecord.ToString());
+	}
 }
