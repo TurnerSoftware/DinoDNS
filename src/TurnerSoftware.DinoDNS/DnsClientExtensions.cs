93b4d771b54d2ac34c5ab6e333a7f874972c9e63
Turnerj <git@turnerj.com>
2022-02-17 21:37:02 +1030

    Add helper methods

---
+++ src/TurnerSoftware.DinoDNS/DnsClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using TurnerSoftware.DinoDNS.Protocol;
+
+namespace TurnerSoftware.DinoDNS;
+
+public static class DnsClientExtensions
+{
+	private static ushort GetRandomIdentifier() => (ushort)Random.Shared.Next(ushort.MaxValue);
+
+	public static async ValueTask<DnsMessage> QueryAsync(this DnsClient client, string query, DnsQueryType type, DnsClass dnsClass = DnsClass.IN, CancellationToken cancellationToken = default) 
+		=> await client.QueryAsync(new Question(query, type, dnsClass), cancellationToken);
+
+	public static async ValueTask<DnsMessage> QueryAsync(this DnsClient client, Question question, CancellationToken cancellationToken = default) => await client.SendAsync(
+		DnsMessage.CreateQuery(GetRandomIdentifier()).WithQuestions(new[] { question }),
+		cancellationToken
+	);
+}
