86afb3bc9f5a956ebb031b4be2ccc974e88dde2e
Turnerj <git@turnerj.com>
2022-02-19 16:43:27 +1030

    Remove test code from benchmarks

--- benchmarks/TurnerSoftware.DinoDNS.Benchmarks/Program.cs
+++ benchmarks/TurnerSoftware.DinoDNS.Benchmarks/Program.cs
@@ -1,14 +1,3 @@
 ﻿using BenchmarkDotNet.Running;
-using TurnerSoftware.DinoDNS.Benchmarks;
-
-
-// There seems to be a small bug with processing the received data via TCP - pretty sure the issue is with the TcpConnection!
-
-//var a = new FullStackBenchmark();
-//a.Setup();
-//TestServer.Start(new[] {"tcp"});
-//await a.Kapetan_DNS();
-//await a.Kapetan_DNS();
-//a.Cleanup();
 
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
