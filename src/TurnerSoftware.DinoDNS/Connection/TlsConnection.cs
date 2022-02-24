d3dd35fb6b05c0c5b89ad89fdd8f098a2c978beb
Turnerj <git@turnerj.com>
2022-02-24 22:36:15 +1030

    Overhaul of the TLS client and server

--- src/TurnerSoftware.DinoDNS/Connection/TlsConnection.cs
+++ src/TurnerSoftware.DinoDNS/Connection/TlsConnection.cs
@@ -4,31 +4,42 @@ using System.Net.Security;
 using System.Security.Authentication;
 using System.Collections.Concurrent;
 using System.Net;
+using System.Buffers;
 
 namespace TurnerSoftware.DinoDNS.Connection;
 
 public sealed class TlsConnectionClient : TcpConnectionClient
 {
-	public const SslProtocols DefaultSslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13;
-	public new static readonly TlsConnectionClient Instance = new(DefaultSslProtocols);
+	public sealed class AuthOptions
+	{
+		public static readonly Func<EndPoint, SslClientAuthenticationOptions> Default = static _ => new();
 
-	public readonly SslProtocols EnabledSslProtocols;
+		public static readonly Func<EndPoint, SslClientAuthenticationOptions> DoNotValidate = static _ => new()
+		{
+			RemoteCertificateValidationCallback = (_, _, _, _) => true
+		};
+	}
+
+	public new static readonly TlsConnectionClient Instance = new(AuthOptions.Default);
+
+	private readonly Func<EndPoint, SslClientAuthenticationOptions> AuthOptionsFactory;
 
 	private readonly ConcurrentDictionary<IntPtr, SslStream> StreamLookup = new();
 
-	public TlsConnectionClient(SslProtocols enabledSslProtocols)
+	public TlsConnectionClient(Func<EndPoint, SslClientAuthenticationOptions> authOptionsFactory)
 	{
-		EnabledSslProtocols = enabledSslProtocols;
+		AuthOptionsFactory = authOptionsFactory;
 	}
 
 	protected override async ValueTask OnConnectAsync(Socket socket, IPEndPoint endPoint, CancellationToken cancellationToken)
 	{
 		var stream = new SslStream(new NetworkStream(socket), true);
-		await stream.AuthenticateAsClientAsync(new SslClientAuthenticationOptions
+		var socketSpecificAuthOptions = AuthOptionsFactory(socket.RemoteEndPoint!);
+		if (socketSpecificAuthOptions.TargetHost is null)
 		{
-			TargetHost = socket.RemoteEndPoint!.ToString(),
-			EnabledSslProtocols = EnabledSslProtocols
-		}, cancellationToken).ConfigureAwait(false);
+			socketSpecificAuthOptions.TargetHost = endPoint.ToString();
+		}
+		await stream.AuthenticateAsClientAsync(socketSpecificAuthOptions, cancellationToken).ConfigureAwait(false);
 		StreamLookup.TryAdd(socket.Handle, stream);
 	}
 
@@ -57,5 +68,51 @@ public sealed class TlsConnectionClient : TcpConnectionClient
 
 public sealed class TlsConnectionServer : TcpConnectionServer
 {
+	public readonly SslProtocols EnabledSslProtocols;
 
+	private readonly ConcurrentDictionary<IntPtr, SslStream> StreamLookup = new();
+	private readonly SslServerAuthenticationOptions Options;
+
+	public TlsConnectionServer(SslServerAuthenticationOptions options)
+	{
+		Options = options;
+	}
+
+	protected override async ValueTask OnConnectAsync(Socket socket, CancellationToken cancellationToken)
+	{
+		var stream = new SslStream(new NetworkStream(socket), true);
+		await stream.AuthenticateAsServerAsync(Options, cancellationToken).ConfigureAwait(false);
+		StreamLookup.TryAdd(socket.Handle, stream);
+	}
+
+	protected override void OnSocketEnd(Socket socket) => StreamLookup.TryRemove(socket.Handle, out _);
+
+	protected override async ValueTask<int> ReadRequestAsync(Socket socket, Memory<byte> requestBuffer, CancellationToken cancellationToken)
+	{
+		var stream = StreamLookup.GetValueOrDefault(socket.Handle)!;
+		//Read the corresponding 2-byte length in the request to know how long the message is
+		await stream.ReadAsync(requestBuffer[..2], cancellationToken).ConfigureAwait(false);
+		var messageLength = BinaryPrimitives.ReadUInt16BigEndian(requestBuffer.Span);
+		//Read the request based on the determined message length
+		await stream.ReadAsync(requestBuffer[..messageLength], cancellationToken).ConfigureAwait(false);
+		return messageLength;
+	}
+
+	protected override async ValueTask WriteResponseAsync(Socket socket, ReadOnlyMemory<byte> responseBuffer, CancellationToken cancellationToken)
+	{
+		var tempBuffer = ArrayPool<byte>.Shared.Rent(2);
+		try
+		{
+			var stream = StreamLookup.GetValueOrDefault(socket.Handle)!;
+			//TCP connections require sending a 2-byte length value before the message.
+			BinaryPrimitives.WriteUInt16BigEndian(tempBuffer.AsSpan(), (ushort)responseBuffer.Length);
+			await stream.WriteAsync(tempBuffer.AsMemory(0, 2), cancellationToken).ConfigureAwait(false);
+			//Send the response message.
+			await stream.WriteAsync(responseBuffer, cancellationToken).ConfigureAwait(false);
+		}
+		finally
+		{
+			ArrayPool<byte>.Shared.Return(tempBuffer);
+		}
+	}
 }
