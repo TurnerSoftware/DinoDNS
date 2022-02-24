eb2cc3ecdb7b95e493c3a2cc1fdf6ffb0572c384
Turnerj <git@turnerj.com>
2022-02-24 22:37:55 +1030

    Replace existing test server with new one

--- benchmarks/StaticDnsServer/Program.cs
+++ benchmarks/StaticDnsServer/Program.cs
@@ -1,3 +1,47 @@
-﻿using TurnerSoftware.DinoDNS.Benchmarks;
+﻿using TurnerSoftware.DinoDNS.Connection;
+using TurnerSoftware.DinoDNS.Protocol;
+using TurnerSoftware.DinoDNS.TestServer;
 
-await TestServer.StartAsync(args);
+try
+{
+	var cancellationTokenSource = new CancellationTokenSource();
+	_ = Task.Run(() => {
+		Console.Read();
+		cancellationTokenSource.Cancel();
+	});
+
+	IDnsConnectionServer? server = null;
+	var protocol = args.Length > 0 ? args[0] : "udp";
+	switch (protocol)
+	{
+		case "tcp":
+			Console.WriteLine("TCP server started!");
+			server = TcpConnectionServer.Instance;
+			break;
+		case "tls":
+			Console.WriteLine("TLS server started!");
+			server = new TlsConnectionServer(new System.Net.Security.SslServerAuthenticationOptions
+			{
+				ServerCertificate = DnsTestServer.CreateTemporaryCertificate()
+			});
+			break;
+		case "udp":
+		default:
+			Console.WriteLine("UDP server started!");
+			server = UdpConnectionServer.Instance;
+			break;
+	}
+
+	await DnsTestServer.Instance.StartAsync(
+		server, 
+		request => DnsMessage.CreateResponse(request, ResponseCode.NOERROR) with
+		{
+			Answers = DnsTestServer.ExampleData.Response.Answers
+		},
+		cancellationTokenSource.Token
+	);
+}
+catch (OperationCanceledException)
+{
+	Console.WriteLine("Benchmark server has ended.");
+}
