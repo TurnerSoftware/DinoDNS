7886088502588e9f09e63cd11eb19039b57f09c7
Turnerj <git@turnerj.com>
2022-02-24 22:37:31 +1030

    Added test for TLS client/server

---
+++ tests/TurnerSoftware.DinoDNS.Tests/Connection/TlsConnectionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TurnerSoftware.DinoDNS.Connection;
+using TurnerSoftware.DinoDNS.Protocol;
+using TurnerSoftware.DinoDNS.TestServer;
+
+namespace TurnerSoftware.DinoDNS.Tests.Connection;
+
+[TestClass]
+public class TlsConnectionTests
+{
+	public static IDisposable RunTestServer(Func<DnsMessage, DnsMessage> getResponse)
+	{
+		return DnsTestServer.Instance.Run(new TlsConnectionServer(new System.Net.Security.SslServerAuthenticationOptions
+		{
+			ServerCertificate = DnsTestServer.CreateTemporaryCertificate()
+		}), getResponse);
+	}
+
+	[TestMethod]
+	public async Task BasicTls()
+	{
+		using var _ = RunTestServer(request => DnsTestServer.ExampleData.Response);
+
+		var client = new DnsClient(new NameServer[]
+		{
+			new(DnsTestServer.ClientEndPoint, new TlsConnectionClient(TlsConnectionClient.AuthOptions.DoNotValidate)) 
+		}, DnsMessageOptions.Default);
+
+		var response = await client.SendAsync(DnsTestServer.ExampleData.Request);
+		Assert.AreEqual(DnsTestServer.ExampleData.Response, response);
+	}
+}
