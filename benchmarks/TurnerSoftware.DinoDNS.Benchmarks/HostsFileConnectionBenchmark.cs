f4ba072f477e561d144c85fde6649c3111a76a98
Turnerj <git@turnerj.com>
2022-03-25 21:33:52 +1030

    Add benchmark for hosts file client connection

---
+++ benchmarks/TurnerSoftware.DinoDNS.Benchmarks/HostsFileConnectionBenchmark.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using BenchmarkDotNet.Attributes;
+using TurnerSoftware.DinoDNS.Connection;
+using TurnerSoftware.DinoDNS.Protocol;
+using TurnerSoftware.DinoDNS.TestServer;
+
+namespace TurnerSoftware.DinoDNS.Benchmarks;
+
+[Config(typeof(DefaultBenchmarkConfig))]
+public class HostsFileConnectionBenchmark
+{
+	private HostsFileConnectionClient? Client { get; set; }
+
+	private readonly IPEndPoint Endpoint = IPEndPoint.Parse("127.0.0.1:53");
+	private ReadOnlyMemory<byte> RequestMessage;
+	private Memory<byte> ResponseMessage;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		var hostsFile = new DnsHostsFile();
+		Client = new HostsFileConnectionClient(hostsFile);
+		hostsFile.Add("test.www.example.org", new byte[] { 192, 168, 0, 1 });
+
+		var data = new byte[1024].AsMemory();
+		var bytesWritten = DnsTestServer.ExampleData.Request.WriteTo(data);
+		RequestMessage = data[..bytesWritten];
+
+		ResponseMessage = new byte[1024];
+	}
+
+	[Benchmark]
+	public async Task DinoDNS()
+	{
+		await Client!.SendMessageAsync(Endpoint, RequestMessage, ResponseMessage, default);
+	}
+}
